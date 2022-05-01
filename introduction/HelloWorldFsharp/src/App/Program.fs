5d10386ff0ce68a89993785f8b74f363d24806bf
Tiago Onofre <taraujonofre@gmail.com>
2022-04-30 22:04:20 -0300

    Reviewing how to start a project and run

---
+++ introduction/HelloWorldFsharp/src/App/Program.fs
@@ -0,0 +1,9 @@
+﻿open System
+
+[<EntryPoint>]
+let main argv = // string [] -> int
+    printfn "Hello from F#"
+    let str_with_args = String.concat " " argv |> 
+                        fun concatened_args -> "Argv input -> " + concatened_args
+    in
+    printfn "%s" str_with_args |> fun () -> 0
