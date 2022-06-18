681765f386a352b54653feefc065a2f05f03518b
Tiago Onofre <taraujonofre@gmail.com>
2022-06-17 21:55:53 -0300

    feat: start sequences

---
+++ Sequences/Sequences/Program.fs
@@ -0,0 +1,12 @@
+﻿open System
+
+[<EntryPoint>]
+let main argv = 
+    
+    let total =
+        seq { for i in 1..1000 -> i * i }
+        |> Seq.sum
+    
+    printfn "The total is: %i" total
+
+    0
